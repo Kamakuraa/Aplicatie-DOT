6e9397a388e44c77a4071cb4f2f6c33f8b2cf156
alexbratu <a.cosmin.bratu@gmail.com>
2022-09-28 14:35:44 +0300

    new

--- src/main/java/ro/rarom/aplicatiedot/data/DataLoader.java
+++ src/main/java/ro/rarom/aplicatiedot/data/DataLoader.java
@@ -1,2 +1,30 @@
-package ro.rarom.aplicatiedot.data;public class DataLoader {
+package ro.rarom.aplicatiedot.data;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import ro.rarom.aplicatiedot.entity.CereriClientiEntity;
+import ro.rarom.aplicatiedot.repository.CereriClientiRepository;
+
+import java.util.List;
+
+//@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+  private final CereriClientiRepository repo;
+
+  @Override
+  public void run(String... args) throws Exception {
+//    Fairy fairy = Fairy.create ();
+//
+//    repo.saveAll (IntStream.range (0, 100)
+//      .mapToObj (index -> fairy.person ())
+//      .map (person -> new CereriClientiEntity (person.getFullName (), person.getAge ()))
+//      .toList ());
+    repo.saveAll (List.of (
+      new CereriClientiEntity ("something", 11),
+      new CereriClientiEntity ("another", 21),
+      new CereriClientiEntity ("something", 4)
+    ));
+  }
 }
